4ce6e2070ac23b281a7e436f40145fcd5722e290
T-Zocker <tzoockeer@gmail.com>
2021-06-02 14:49:22 +0200

    Added createQuader() funktion

--- Main.java
+++ Main.java
@@ -6,12 +6,28 @@ public class Main
     GLLicht light;
     GLTastatur keyboard;
     
+    public static GLQuader createQuader(double posX, double posY, double posZ, double lenght, double height, double width) {
+        GLQuader quader = new GLQuader(posX, posY, posZ, lenght, height, width);
+        quader.setzeSelbstleuchten(255, 255, 255);
+        return quader;
+    }
+    
     public Main()
     {
         // Innitializing of the Objects
-        cam = new GLSchwenkkamera();
+        cam = new GLEntwicklerkamera();
         keyboard = new GLTastatur();
         light = new GLLicht();
         
+        while(true) {
+            if(keyboard.wurdeGedrueckt()) {
+                switch(keyboard.gibZeichen()) {
+                    case 'C':
+                    case 'c':
+                        createQuader(0, 0, 0, 10, 10, 10);
+                        break;
+                }
+            }
+        }
     }
 }
