7a835b698ae7d7dc1b94e7ab1d0c93f874ee9bf8
T-Zocker <tzoockeer@gmail.com>
2021-06-03 14:56:17 +0200

    Removed redundent comment

--- Main.java
+++ Main.java
@@ -5,22 +5,21 @@ public class Main
     GLKamera cam;
     GLLicht light;
     GLTastatur keyboard;
-    
+
     public static GLQuader createQuader(double posX, double posY, double posZ, double lenght, double height, double width) {
         GLQuader quader = new GLQuader(posX, posY, posZ, lenght, height, width);
         quader.setzeSelbstleuchten(255, 255, 255);
         return quader;
     }
-    
+
     public Main()
     {
         // Innitializing of the Objects
         cam = new GLEntwicklerkamera();
-        // Innitializing the Objects
         cam = new GLSchwenkkamera();
         keyboard = new GLTastatur();
         light = new GLLicht();
-        
+
         while(true) {
             if(keyboard.wurdeGedrueckt()) {
                 switch(keyboard.gibZeichen()) {
