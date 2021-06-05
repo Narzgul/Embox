1bdeaf86ae590b268d8bed9799722c5091caa39d
T-Zocker <tzoockeer@gmail.com>
2021-06-05 15:53:42 +0200

    Added a Util.java interface to but all useful methods in

--- CreateMenu.java
+++ CreateMenu.java
@@ -12,18 +12,26 @@ public class CreateMenu {
             JPanel panel = new JPanel();
             panel.setLayout(new GridLayout(0,1));
 
-            String[] items = {"Cube", "Sphere", "Torus"};
+            String[] items = {"Cube", "Sphere", "Torus", "Cone", "Truncated Cone", "Cylinder"};
             JComboBox<String> cb = new JComboBox<>(items);
             panel.add(cb);
 
             JButton button = new JButton("Create");
             button.addActionListener(actionEvent -> {
                 switch (Objects.requireNonNull(cb.getSelectedItem()).toString().toLowerCase(Locale.ROOT)) {
-                    case "cube": Main.createCuboid(0, 0, 0, 10, 10, 10);
+                    case "cube": Util.createCuboid(0, 0, 0, 10, 10, 10);
                         break;
-                    case "sphere": Main.createSphere(0, 0, 0, 10);
+                    case "sphere": Util.createSphere(0, 0, 0, 10);
                         break;
-                    default: System.out.println("Neither cube nor sphere");
+                    case "torus": Util.createTorus(0, 0, 0, 10, 10);
+                        break;
+                    case "cone": Util.createCone(0, 0, 0, 10, 10);
+                        break;
+                    case "truncated cone": Util.createTruncatedCone(0, 0, 0, 20, 10, 15);
+                        break;
+                    case "cylinder": Util.createCylinder(0, 0, 0, 10, 20);
+                        break;
+                    default: System.out.println("Tried to create not handheld object");
                 }
                 frame.dispose();
             });
