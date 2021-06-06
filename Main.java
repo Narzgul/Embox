423cfc274f5e8868d68e311b6515a61d5007e346
T-Zocker <tzoockeer@gmail.com>
2021-06-06 16:29:01 +0200

    Adds all objects to the list

--- Main.java
+++ Main.java
@@ -1,9 +1,13 @@
 import GLOOP.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     GLKamera cam;
     GLLicht light;
     GLTastatur keyboard;
+    public static List<GLObjekt> objectList = new ArrayList<>();
 
     public Main() {
         // Initializing of the Objects
--- Util.java
+++ Util.java
@@ -4,36 +4,42 @@ public interface Util {
     static GLQuader createCuboid(double posX, double posY, double posZ, double length, double height, double width) {
         GLQuader cuboid = new GLQuader(posX, posY, posZ, length, height, width);
         cuboid.setzeSelbstleuchten(255, 255, 255);
+        Main.objectList.add(cuboid);
         return cuboid;
     }
 
     static GLKugel createSphere(double posX, double posY, double posZ, double radius) {
         GLKugel sphere = new GLKugel(posX, posY, posZ, radius);
         sphere.setzeSelbstleuchten(255, 255, 255);
+        Main.objectList.add(sphere);
         return sphere;
     }
 
     static GLTorus createTorus(double posX, double posY, double posZ, double radius, double thickness) {
         GLTorus torus = new GLTorus(posX, posY, posZ, radius, thickness);
         torus.setzeSelbstleuchten(255, 255, 255);
+        Main.objectList.add(torus);
         return torus;
     }
 
     static GLZylinder createCylinder(double posX, double posY, double posZ, double radius, double height) {
         GLZylinder cylinder = new GLZylinder(posX, posY, posZ, radius, height);
         cylinder.setzeSelbstleuchten(255, 255, 255);
+        Main.objectList.add(cylinder);
         return cylinder;
     }
 
     static GLKegelstumpf createTruncatedCone(double posX, double posY, double posZ, double radius1, double radius2, double height) {
-        GLKegelstumpf torus = new GLKegelstumpf(posX, posY, posZ, radius1, radius2, height);
-        torus.setzeSelbstleuchten(255, 255, 255);
-        return torus;
+        GLKegelstumpf truncatedCone = new GLKegelstumpf(posX, posY, posZ, radius1, radius2, height);
+        truncatedCone.setzeSelbstleuchten(255, 255, 255);
+        Main.objectList.add(truncatedCone);
+        return truncatedCone;
     }
 
     static GLKegel createCone(double posX, double posY, double posZ, double radius, double height) {
-        GLKegel torus = new GLKegel(posX, posY, posZ, radius, height);
-        torus.setzeSelbstleuchten(255, 255, 255);
-        return torus;
+        GLKegel cone = new GLKegel(posX, posY, posZ, radius, height);
+        cone.setzeSelbstleuchten(255, 255, 255);
+        Main.objectList.add(cone);
+        return cone;
     }
 }
